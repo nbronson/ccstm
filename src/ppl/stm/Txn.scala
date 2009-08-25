c36b2622c1dceccefe964b283e34fd786e714d71
Nathan Bronson <nbronson@flowertower.(none)>
2009-08-24 23:57:33 -0700

    Work on Txn interface.

--- src/ppl/stm/Txn.scala
+++ src/ppl/stm/Txn.scala
@@ -7,54 +7,124 @@ package ppl.stm
 
 object Txn {
 
-  sealed abstract class Status
-  case object Active extends Status
-  case object MarkedRollback extends Status
-  case object Preparing extends Status
-  case object Prepared extends Status
-  case object Committing extends Status
-  case object Committed extends Status
-  case object RollingBack extends Status
-  case object Rolledback extends Status
+  /** Represents the current status of a <code>Txn</code>. */
+  sealed abstract class Status(mayCommit0: Boolean, willCommit0: Boolean) {
+    /** True if a transaction with this status may eventually commit, false if
+     *  rollback is inevitable.
+     */
+    val mayCommit = mayCommit0
+
+    /** True if a transaction with this status has decided on commit, false if
+     *  rollback may still occur.
+     */
+    val willCommit = willCommit0
+  }
+
+  /** The <code>Status</code> for a <code>Txn</code> that may commit, but for
+   *  which completion has not yet been requested.
+   */
+  case object Active extends Status(true, false)
+
+  /** The <code>Status</code> for a <code>Txn</code> that will not commit, but
+   *  for which completion has not yet been requested. 
+   */
+  case object MarkedRollback extends Status(false, false)
+
+  /** The <code>Status</code> for a <code>Txn</code> that may commit and whose
+   *  completion has been requested, but that has not yet acquired the
+   *  resources required to guarantee that commit is possible.
+   */
+  case object Preparing extends Status(true, false)
+
+  /** The <code>Status</code> for a <code>Txn</code> that is guaranteed to
+   *  commit, but that has not yet released all of its resources.
+   */
+  case object Committing extends Status(true, true)
+
+  /** The <code>Status</code> for a <code>Txn</code> that has successfully
+   *  committed, applying all of its changes.
+   */
+  case object Committed extends Status(true, true)
+
+  /** The <code>Status</code> for a <code>Txn</code> that will definitely roll
+   *  back, but that has not yet released all of its resources.  A rollback is
+   *  considered to be implicit if <code>Txn.requireRollback</code> was called
+   *  during a lifecycle handler's invocation, explicit otherwise.
+   */
+  case class RollingBack(explicit: Boolean, cause: Any) extends Status(false, false)
+
+  /** The <code>Status</code> for a <code>Txn</code> that has been completely
+   *  rolled back.  A rollback is considered to be implicit if
+   *  <code>Txn.triggerRollback</code> was called during a lifecycle handler's
+   *  invocation, explicit otherwise.
+   */
+  case class Rolledback(explicit: Boolean, cause: Any) extends Status(false, false)
+
 
   trait ReadSetCallback {
-    /** Called during the Active and Prepared states.  Validation during the
-     *  Preparing state may be skipped if no other transactions have committed
-     *  since the last validation, or if no WriteSetCallback-s have been
-     *  registered.
+    /** Called during the <code>Active</code> and/or <code>Prepared</code>
+     *  states.  Validation during the <code>Preparing</code> state may be
+     *  skipped if no other transactions have committed since the last
+     *  validation, or if no <code>WriteSetCallback</code>s have been
+     *  registered.  Implementations should call
+     *  <code>txn.requireRollback</code> if <code>txn</code> is no longer
+     *  valid.
      */
     def validate(txn: Txn)
   }
 
-  trait WriteSetCallback extends CommitHandler with RollbackHandler {
-    /** Called during the Preparing states.  All locks required to complete
-     *  the commit must be acquired during this callback, or else
-     *  txn.markForRollback must be called.
+  trait WriteSetCallback {
+    /** Called during the <code>Preparing</code> state.  All locks or other
+     *  resources required to complete the commit must be acquired during this
+     *  callback, or else <code>txn.requireRollback</code> must be called.
      */
     def prepare(txn: Txn)
-  }
 
-  trait CommitHandler {
-    /** Called during the Committing state. */
+    /** Called during the <code>Committing</code> state. */
     def performCommit(txn: Txn)
-  }
 
-  trait RollbackHandler {
-    /** Called during the RollingBack state. */
+    /** Called during the <code>RollingBack</code> state. */
     def performRollback(txn: Txn)
   }
 
   trait Synchronization {
-    /** Called during the Active and MarkedRollback states, after commit has been requested. */
+    /** Called during the <code>Active</code> or <code>MarkedRollback</code>
+     *  states, after completion has been requested.
+     */
     def beforeCompletion(txn: Txn)
 
-    /** Called during the Committed and Rolledback states. */
+    /** Called during the <code>Committed</code> or <code>Rolledback</code>
+     *  states.
+     */
     def afterCompletion(txn: Txn)
+  }
+
+  abstract class AbstractTxn {
+    /** Returns the transaction's current status, as of the most recent
+     *  operation.  Does not validate the transaction.
+     */
+    def status: Status
+
+    /** Validates that the transaction is consistent with all other committed
+     *  transactions and completed non-transactional accesses, throwing
+     *  <code>RollbackException</code> if this transaction is not consistent.
+     *  @throws ppl.stm.RollbackException if this transaction cannot commit.
+     */
+    def validate { if (!validatedStatus.mayCommit) throw RollbackException }
+
+    /** Validates that the transaction is consistent with all other committed
+     *  transactions and completed non-transactional accesses, setting the
+     *  status to <code>RollingBack</code> if the transaction is inconsistent,
+     *  then returns the current status.
+     */
+    def validatedStatus: Status
   }
 }
 
 class Txn extends STM.TxnImpl {
   import Txn._
+
+
 
   def valid: Boolean = false
 
---
+++ src/ppl/stm/impls/IndirectEagerTL2.scala
@@ -0,0 +1,252 @@
+/* $Id$
+ *
+ * Copyright 2009 Nathan Bronson and Stanford University.
+ */
+
+package ppl.stm.impls
+
+import ppl.stm.TVar
+import ppl.stm.UnrecordedRead
+
+/** An STM implementation that uses a TL2-style timestamp system, but that
+ *  performs eager acquisition of write locks and that associates version
+ *  metadata with each value via an immutable wrapper.
+ */
+trait IndirectEagerTL2 {
+  type Metadata = Unit
+  type MetadataHolder = UnitMetadataHolder
+  type TxnAccessor[T] = IndirectEagerTL2TxnAccessor[T]
+  type NonTxnAccessor[T] = IndirectEagerTL2NonTxnAccessor[T]
+  type TxnImpl = IndirectEagerTL2Txn
+}
+
+private [tl2] object IndirectEagerTL2 {
+
+  abstract class Wrapped[T](val value: T, val version: Long) {
+    def owner: IndirectEagerTL2Txn
+  }
+
+  case class Unlocked[T]()
+
+  case class Changing[T](txn: IndirectEagerTL2Txn, before: T, after: T) {
+    def elem = if (txn.committed) after else before
+  }
+
+  type Version = Long
+
+  val VersionChanging: Version = 1L
+
+  /** The fundamental correctness assumption of the system is that if txn R
+   *  observes {@link #globalVersion} to be
+   */
+  @volatile var globalVersion: Version = 0
+
+  def nextNonTxnWriteVersion(): Version = 0
+}
+
+class IndirectEagerTL2Txn {
+  def committed = false
+}
+
+abstract class IndirectEagerTL2TxnAccessor[T] extends TVar.Bound[T] {
+  import IndirectEagerTL2._
+
+  def field: Int
+  val txn: Txn
+  var metadata: Version
+  var data: Any
+
+  var _addedToReadSet = false
+  var _addedToWriteSet = false
+
+  def context: Option[Txn] = Some(txn)
+
+//  def elem: T = {
+//    var attempts = 0
+//    while (attempts < 100) {
+//    }
+//
+//    null.asInstanceOf[T]
+//  }
+//
+//  private def attemptRead(addToRS: Boolean): Option[T] = {
+//    val v = metadata
+//    val d = data
+//    if (v != metadata) return None
+//
+//    if (v > txn.readVersion && !txn.revalidate(v)) throw RollbackException
+//
+//    val z = (if ((v & Txn.VersionChanging) != 0) {
+//      // Somebody is changing _data.  If they have installed a Txn.Changing
+//      // instance then we can choose to grab the pre-change value and
+//      // continue, because we may commit before them.  If no Txn.Changing
+//      // instance is available then the situation will quickly change,
+//      // because threads do little work in that state, or we are very
+//      // unlucky and the blocking thread has been preempted.
+//      d match {
+//        case c: Txn.Changing[T] => {
+//          if (txn == c.txn) return Some(c.after)
+//          c.before
+//        }
+//        case _ => return None
+//      }
+//    } else {
+//      assert(!d.isInstanceOf[Txn.Changing[_]])
+//      d.asInstanceOf[T]
+//    })
+//
+//    // read is stable and consistent, should we add it to the read set?
+//    if (addToRS) {
+//      _addedToReadSet = true
+//      txn.addToReadSet(this)
+//    }
+//  }
+//
+//  def elemMap[Z](f: (T) => Z): Z = {
+//    if (_addedToReadSet || _addedToWriteSet) {
+//      // no benefit to reexecution of f later
+//      f(elem)
+//    } else {
+//
+//    }
+//  }
+//
+//  def elem_=(newValue: T) {
+//    while (true) {
+//      (TVar.this.synchronized {
+//        data match {
+//          case d: Txn.Changing[_] => d.txn
+//          case _ => {
+//            _version |= Txn.VersionChanging
+//            data = newValue
+//            _version = Txn.nextNonTxnWriteVersion()
+//            return
+//          }
+//        }
+//      }).awaitCompletion()
+//    }
+//  }
+//
+//  def transform(f: (T) => T) {
+//    while (true) {
+//      (TVar.this.synchronized {
+//        data match {
+//          case d: Txn.Changing[_] => d.txn
+//          case d => {
+//            val newValue = f(d.asInstanceOf[T])
+//            _version |= Txn.VersionChanging
+//            data = newValue
+//            _version = Txn.nextNonTxnWriteVersion()
+//            return
+//          }
+//        }
+//      }).awaitCompletion()
+//    }
+//  }
+//
+//  def compareAndSet(before: T, after: T): Boolean = {
+//    while (true) {
+//      (TVar.this.synchronized {
+//        data match {
+//          case d: Txn.Changing[_] => d.txn
+//          case d => {
+//            if (before != d.asInstanceOf[T]) return false
+//            _version |= Txn.VersionChanging
+//            data = after
+//            _version = Txn.nextNonTxnWriteVersion()
+//            return true
+//          }
+//        }
+//      }).awaitCompletion()
+//    }
+//    throw new Error("unreachable")
+//  }
+
+  def elem: T = null.asInstanceOf[T]
+
+  def unrecordedRead: UnrecordedRead[T] = null
+
+  def elem_=(v: T) {}
+
+  def transformIfDefined(pf: PartialFunction[T,T]): Boolean = false
+}
+
+abstract class IndirectEagerTL2NonTxnAccessor[T] extends TVar.Bound[T] {
+  import IndirectEagerTL2._
+
+  var metadata: Version
+  var data: Any
+
+  def context: Option[Txn] = None
+
+
+//  def elem: T = {
+//    data match {
+//      case d: Txn.Changing[_] => d.asInstanceOf[Txn.Changing[T]].elem
+//      case d => d.asInstanceOf[T]
+//    }
+//  }
+//
+//  def elemMap[Z](f: (T) => Z): Z = f(elem)
+//
+//  def elem_=(newValue: T) {
+//    while (true) {
+//      (TVar.this.synchronized {
+//        data match {
+//          case c: Txn.Changing[_] => c.txn
+//          case _ => {
+//            _version |= Txn.VersionChanging
+//            data = newValue
+//            _version = Txn.nextNonTxnWriteVersion()
+//            return
+//          }
+//        }
+//      }).awaitCompletion()
+//    }
+//  }
+//
+//  def transform(f: (T) => T) {
+//    while (true) {
+//      (TVar.this.synchronized {
+//        data match {
+//          case c: Txn.Changing[_] => c.txn
+//          case v => {
+//            val newValue = f(v.asInstanceOf[T])
+//            _version |= Txn.VersionChanging
+//            data = newValue
+//            _version = Txn.nextNonTxnWriteVersion()
+//            return
+//          }
+//        }
+//      }).awaitCompletion()
+//    }
+//  }
+//
+//  def compareAndSet(before: T, after: T): Boolean = {
+//    while (true) {
+//      (TVar.this.synchronized {
+//        data match {
+//          case c: Txn.Changing[_] => c.txn
+//          case v => {
+//            if (before != v.asInstanceOf[T]) return false
+//            _version |= Txn.VersionChanging
+//            data = after
+//            _version = Txn.nextNonTxnWriteVersion()
+//            return true
+//          }
+//        }
+//      }).awaitCompletion()
+//    }
+//    throw new Error("unreachable")
+//  }
+
+
+  def elem: T = null.asInstanceOf[T]
+
+  def unrecordedRead: UnrecordedRead[T] = null
+
+  def elem_=(v: T) {}
+
+  def transformIfDefined(pf: PartialFunction[T,T]): Boolean = false
+}
+
