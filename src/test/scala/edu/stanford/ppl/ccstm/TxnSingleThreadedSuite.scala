f81099ca6f25b49f23c6d7463ca0a02b7227fead
Nathan Bronson <nbronson@flowertower.(none)>
2009-09-14 10:57:24 -0700

    More tests.

--- src/test/scala/edu/stanford/ppl/ccstm/TxnSingleThreadedSuite.scala
+++ src/test/scala/edu/stanford/ppl/ccstm/TxnSingleThreadedSuite.scala
@@ -7,7 +7,7 @@ package edu.stanford.ppl.ccstm
 
 import org.scalatest.FunSuite
 
-class TxnSuite extends FunSuite {
+class TxnSingleThreadedSuite extends FunSuite {
 
   test("Empty transaction") {
     new Atomic { def body {
@@ -18,7 +18,9 @@ class TxnSuite extends FunSuite {
   test("Counter increment") {
     val x = TVar(1)
     new Atomic { def body {
+      assert(!x == 1)
       x := !x + 1
+      assert(!x == 2)
     }}.run
     assert(x.nonTxn.elem == 2)
   }
@@ -26,7 +28,7 @@ class TxnSuite extends FunSuite {
   test("Counter increment should be fast") {
     val x = TVar(1)
     var best = java.lang.Long.MAX_VALUE
-    for (pass <- 0 until 10000) {
+    for (pass <- 0 until 100000) {
       val begin = System.nanoTime
       for (i <- 0 until 10) {
         new Atomic { def body {
@@ -36,10 +38,24 @@ class TxnSuite extends FunSuite {
       val elapsed = System.nanoTime - begin
       best = best min elapsed
     }
-    assert(x.nonTxn.elem == 100001)    
+    assert(x.nonTxn.elem == 1000001)
     println("best was " + (best / 10.0) + " nanos/call")
 
-    // we should be able to get less than 2500 nanos, even on a Niagara
-    assert(best / 10 < 2500)
+    // We should be able to get less than 5000 nanos, even on a Niagara.
+    // On most platforms we should be able to do much better than this.
+    assert(best / 10 < 5000)
+  }
+
+  class CustomException extends Exception
+
+  test("Failure atomicity") {
+    val x = new TVar(1)
+    intercept[CustomException] {
+      new Atomic { def body {
+        x := 2
+        throw new CustomException
+      }}.run
+    }
+    assert(x.nonTxn.elem == 1)
   }
 }
