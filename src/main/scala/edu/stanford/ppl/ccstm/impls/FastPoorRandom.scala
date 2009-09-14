71235317c16787176d9040c74cf72bd773235d95
Nathan Bronson <nbronson@flowertower.(none)>
2009-09-14 16:04:01 -0700

    Evaluated a simpler alternative.

--- src/main/scala/edu/stanford/ppl/ccstm/impls/FastPoorRandom.scala
+++ src/main/scala/edu/stanford/ppl/ccstm/impls/FastPoorRandom.scala
@@ -10,6 +10,7 @@ package edu.stanford.ppl.ccstm.impls
  *  returned.
  */
 private[impls] class FastPoorRandom {
+  // TODO: (re)choose the number of slots with a bit more thought
   private def Slots = 1024
   private val states = {
     val z = new Array[Long](Slots)
@@ -23,7 +24,7 @@ private[impls] class FastPoorRandom {
     // adapted from Wikipedia
 
     val s = states(id)
-    
+
     var m_z = s.asInstanceOf[Int]
     var m_w = (s >> 32).asInstanceOf[Int]
 
@@ -34,4 +35,20 @@ private[impls] class FastPoorRandom {
 
     (m_z << 16) + m_w  /* 32-bit result */
   }
+
+  // nbronson: on my laptop the following implementation is 2 nanoseconds faster (11.9 instead of 13.9)
+
+//  private val states = {
+//    val z = new Array[Int](Slots)
+//    for (i <- 0 until Slots) z(i) =  i
+//    z
+//  }
+//
+//  def nextInt: Int = {
+//    val id = Thread.currentThread.hashCode & (Slots - 1)
+//
+//    val s = states(id) * (2000000000 + 1141592653) + 14142135
+//    states(id) = s
+//    s
+//  }
 }
