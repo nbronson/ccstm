7fa3a4cee598615b53c01eed5b984dfad11e5c8f
Nathan Bronson <nbronson@flowertower.(none)>
2009-09-26 13:57:12 -0700

    Added WakeupManager, which associates pendingWakeups metadata by hash code of the source.

---
+++ src/main/scala/edu/stanford/ppl/ccstm/impls/WakeupManager.scala
@@ -0,0 +1,144 @@
+/* CCSTM - (c) 2009 Stanford University - PPL */
+
+// WakeupManager
+
+package edu.stanford.ppl.ccstm.impls
+
+
+import java.util.concurrent.atomic.{AtomicReferenceArray, AtomicLongArray}
+
+private[impls] class WakeupManager(numChannels: Int, numSources: Int) {
+  def this() = this(64, 512)
+
+  assert(numChannels > 0 && numChannels <= 64 && (numChannels & (numChannels - 1)) == 0)
+  assert(numSources > 0 && (numSources & (numSources - 1)) == 0)
+
+  // To reduce false sharing.  Assume 64 byte cache lines and 4 byte pointers.
+  private def ChannelSpacing = 16
+
+  private val pending = new AtomicLongArray(numSources)
+  private val events = new AtomicReferenceArray[Event](numChannels * ChannelSpacing)
+  
+  /** The returned value must later be passed to <code>trigger</code>.
+   *  Multiple return values may be passed to a single invocation of
+   *  <code>trigger</code> by merging them with bitwise-OR.
+   */
+  def prepareToTrigger(ref: AnyRef, offset: Int): Long = {
+    val i = hash(ref, offset) & (numSources - 1)
+    var z = 0L
+    do {
+      z = pending.get(i)
+    } while (z != 0L && !pending.compareAndSet(i, z, 0L))
+    z
+  }
+
+  /** Completes the wakeups started by <code>prepareToTrigger</code>.  If a
+   *  thread completes <code>e = subscribe; e.addSource(r,o)</code> prior to
+   *  a call to <code>prepareToTrigger(r,o)</code> call whose return value is
+   *  included in <code>wakeups</code>, then any pending call to
+   *  <code>e.await</code> will return now and any future calls will return
+   *  immediately.
+   */
+  def trigger(wakeups: Long) {
+    var channel = 0
+    var w = wakeups
+    while (w != 0) {
+      if ((w & 0xffffffffL) == 0) {
+        w >>>= 32
+        channel += 32
+      }
+      if ((w & 0xffffL) == 0) {
+        w >>>= 16
+        channel += 16
+      }
+      if ((w & 0xffL) == 0) {
+        w >>>= 8
+        channel += 8
+      }
+      if ((w & 0xfL) == 0) {
+        w >>>= 4
+        channel += 4
+      }
+      if ((w & 0x3L) == 0) {
+        w >>>= 2
+        channel += 2
+      }
+      if ((w & 0x1L) == 0) {
+        assert((w & 2) != 0)
+        trigger(channel + 1)
+        w >>>= 2
+        channel += 2
+      } else {
+        trigger(channel)
+        w >>>= 1
+        channel += 1
+      }
+    }
+  }
+
+  private def trigger(channel: Int) {
+    val i = channel * ChannelSpacing
+    val e = events.get(i)
+    if (e != null && events.compareAndSet(i, e, null)) e.trigger
+  }
+
+  /** See <code>trigger</code>. */
+  def subscribe: Event = {
+    subscribe(hash(Thread.currentThread, 0) & (numChannels - 1))
+  }
+
+  private def subscribe(channel: Int): Event = {
+    val i = channel * ChannelSpacing
+    while (true) {
+      val existing = events.get(i)
+      if (existing != null) return existing
+      val fresh = new Event(channel)
+      if (events.compareAndSet(i, null, fresh)) return fresh
+    }
+    throw new Error("unreachable")
+  }
+
+  private def hash(ref: AnyRef, offset: Int): Int = {
+    // TODO: merge this with WriteBuffer.hash, where should it go?
+    var h = System.identityHashCode(ref) ^ (0x40108097 * offset)
+    h ^= (h >>> 20) ^ (h >>> 12)
+    h ^= (h >>> 7) ^ (h >>> 4)
+    h
+  }
+
+  class Event(channel: Int) {
+    private val mask = 1L << channel
+    private var triggered = false
+
+    def addSource(ref: AnyRef, offset: Int) {
+      if (!triggered) {
+        val i = hash(ref, offset) & (numSources - 1)
+        var p = 0L
+        do {
+          p = pending.get(i)
+        } while((p & mask) == 0 && !pending.compareAndSet(i, p, p | mask) && !triggered)
+      }
+    }
+
+    def await() {
+      if (!triggered) {
+        synchronized {
+          while (!triggered) {
+            wait
+          }
+        }
+      }
+    }
+
+    private[WakeupManager] def trigger() {
+      if (!triggered) {
+        synchronized {
+          if (!triggered) {
+            triggered = true
+            notifyAll
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/test/scala/edu/stanford/ppl/ccstm/impls/WakeupManagerSuite.scala
@@ -0,0 +1,127 @@
+/* CCSTM - (c) 2009 Stanford University - PPL */
+
+// WakeupManagerSuite
+
+package edu.stanford.ppl.ccstm.impls
+
+
+import java.util.concurrent.CyclicBarrier
+import org.scalatest.FunSuite
+
+class WakeupManagerSuite extends FunSuite {
+  import ParUtil._
+
+  test("single non-blocking") {
+    val mgr = new WakeupManager(1, 1)
+    val event = mgr.subscribe
+    event.addSource("ref", 0)
+    mgr.trigger(mgr.prepareToTrigger("ref", 0))
+    event.await
+  }
+
+  test("duplicate wakeup") {
+    val mgr = new WakeupManager(1, 1)
+    val event = mgr.subscribe
+    event.addSource("ref", 0)
+    mgr.trigger(mgr.prepareToTrigger("ref", 0))
+    mgr.trigger(mgr.prepareToTrigger("ref", 0))
+    event.await
+  }
+
+  test("single blocking") {
+    val mgr = new WakeupManager(1, 1)
+    val event = mgr.subscribe
+    event.addSource("ref", 0)
+    val thread = new Thread {
+      override def run {
+        Thread.sleep(500)
+        mgr.trigger(mgr.prepareToTrigger("ref", 0))
+      }
+    }
+    val t0 = System.currentTimeMillis
+    thread.start
+    event.await
+    val elapsed = System.currentTimeMillis - t0
+    assert(elapsed >= 500)
+    thread.join
+  }
+
+  test("multiple wakeups, early trigger") {
+    runMultipleWakeupTest(0, 500)
+  }
+
+  test("multiple wakeups, late trigger") {
+    runMultipleWakeupTest(0, 500)
+  }
+
+  test("multiple wakeups, racing trigger") {
+    runMultipleWakeupTest(0, 0)
+  }
+
+  private def runMultipleWakeupTest(triggerDelay: Int, awaitDelay: Int) {
+    val mgr = new WakeupManager
+    val barrier = new CyclicBarrier(100)
+    for (id <- 0 par_until 100) {
+      if (id == 0) {
+        barrier.await()
+        Thread.sleep(triggerDelay)
+        var w = 0L
+        for (i <- 1 until 100) {
+          w |= mgr.prepareToTrigger("ref", i)
+        }
+        mgr.trigger(w)
+      }
+      else {
+        val e = mgr.subscribe
+        e.addSource("ref", id)
+        barrier.await()
+        Thread.sleep(awaitDelay)
+        e.await()
+      }
+    }
+  }
+
+  test("single ticket handoff, 2 threads, 100K handoffs/thread") {
+    runTicketTest(1, 2, 100000)
+  }
+
+  test("single ticket handoff, 3 threads, 100K handoffs/thread") {
+    runTicketTest(1, 3, 100000)
+  }
+
+  test("single ticket handoff, 100 threads, 1000 handoffs/thread") {
+    runTicketTest(1, 100, 1000)
+  }
+
+  test("separate ticket handoff, 100 threads, 1000 handoffs/thread") {
+    runTicketTest(50, 2, 1000)
+  }
+
+  test("overlapped ticket handoff, 10 threads, 10K handoffs/thread") {
+    runTicketTest(2, 5, 10000)
+  }
+
+  private def runTicketTest(numTickets: Int, threadsPerTicket: Int, handoffsPerThread: Int) {
+    class TicketHolder {
+      @volatile var turn = 0
+    }
+    val tickets = Array.fromFunction(i => new TicketHolder)(numTickets)
+    var mgr = new WakeupManager
+    val t0 = System.currentTimeMillis
+    for (id <- 0 par_until (numTickets * threadsPerTicket)) {
+      val ticket = tickets(id / threadsPerTicket)
+      val self = id % threadsPerTicket
+      val next = (self + 1) % threadsPerTicket
+      for (h <- 0 until handoffsPerThread) {
+        while (ticket.turn != self) {
+          val e = mgr.subscribe
+          e.addSource(ticket, self)
+          if (ticket.turn != self) e.await
+        }
+        ticket.turn = next
+        mgr.trigger(mgr.prepareToTrigger(ticket, next))
+      }
+    }
+    println((System.currentTimeMillis - t0) + " elapsed (including thread creation/shutdown)")
+  }
+}
