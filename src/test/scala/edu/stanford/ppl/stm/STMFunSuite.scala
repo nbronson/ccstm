474233633094055bcb8010c2e5050f701282f3cc
Nathan Bronson <nbronson@flowertower.(none)>
2009-10-08 18:43:47 -0700

    Revert "Switched to ScalaTest 1.0-RC1."  Not yet compatible with sbt.
    This reverts commit 4650e432ede7efedf387e3f8bd8f333fa12118f4.

--- src/test/scala/edu/stanford/ppl/stm/STMFunSuite.scala
+++ src/test/scala/edu/stanford/ppl/stm/STMFunSuite.scala
@@ -5,13 +5,15 @@
 package edu.stanford.ppl.stm
 
 import edu.stanford.ppl.ccstm.STM
-import org.scalatest.{Tag, Group, FunSuite}
+import org.scalatest.{Group, FunSuite}
+
+
 /** Extends <code>org.scalatest.FunSuite</code> with functionality to make sure
  *  that all transactions that have been assigned slots have released them.
  */
 trait STMFunSuite extends FunSuite {
-  override protected def test(testName: String, testTags: Tag*)(f: => Unit) {
-    super.test(testName, testTags:_*)({
+  override protected def test(testName: String, testGroups: Group*)(f: => Unit) = {
+    super.test(testName, testGroups:_*)({
       f
       STM.Debug.assertQuiescent()
     })
