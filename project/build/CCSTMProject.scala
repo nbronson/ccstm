49984d24c1405e411676b1ac3c1a3cfc1033e109
Nathan Bronson <nbronson@flowertower.(none)>
2009-10-08 18:49:51 -0700

    Revert to ScalaTest 0.9.5 for better SBT integration.

--- project/build/CCSTMProject.scala
+++ project/build/CCSTMProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class CCSTMProject(info: ProjectInfo) extends DefaultProject(info) {
-  val scalatest = "org.scalatest" % "scalatest" % "1.0-SNAPSHOT"
+  val scalatest = "org.scalatest" % "scalatest" % "0.9.5"
 
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
 }
