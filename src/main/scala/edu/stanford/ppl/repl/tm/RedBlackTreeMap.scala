ac7f159d2a86fd4ad21f6d5fa040c275653492d7
Nathan Bronson <nbronson@flowertower.(none)>
2009-10-12 17:44:35 -0700

    get, containsKey, and put implemented.  Switched to property accessor style. Initial results show put() 16x slower than java.util.TreeMap.

--- src/main/scala/edu/stanford/ppl/repl/tm/RedBlackTreeMap.scala
+++ src/main/scala/edu/stanford/ppl/repl/tm/RedBlackTreeMap.scala
@@ -14,30 +14,98 @@ object RedBlackTreeMap {
   private val RED = false
 }
 
-class RedBlackTreeMap[A,B] {
+class RedBlackTreeMap[A <% Ordered[A],B] {
   import RedBlackTreeMap._
 
-  private val root = Ref[Node[A,B]](null)
+  private def root(implicit txn: Txn): Node[A,B] = rootRef.get
+  private def root_=(v: Node[A,B])(implicit txn: Txn) { rootRef.set(v) }
+  private val rootRef = Ref[Node[A,B]](null)
+
+  //////////////// public interface
+
+  def containsKey(key: A)(implicit txn: Txn) = null != getNode(key)
+
+  def get(key: A)(implicit txn: Txn): Option[B] = {
+    getNode(key) match {
+      case null => None
+      case n => Some(n.value)
+    }
+  }
+
+  def put(key: A, value: B)(implicit txn: Txn): Option[B] = {
+    if (null == key) throw new NullPointerException
+
+    var t = root
+    if (null == t) {
+      // easy
+      root = new Node(key, value, null)
+      //TODO: size = 1
+      return None
+    }
+
+    val k: Ordered[A] = key
+    var cmp = 0
+    var parent: Node[A,B] = null
+    do {
+      parent = t
+      cmp = k.compare(t.key)
+      if (cmp < 0) {
+        t = t.left
+      } else if (cmp > 0) {
+        t = t.right
+      } else {
+        return t.setValue(value)
+      }
+    } while (null != t)
+
+    val n = new Node(key, value, parent)
+    if (cmp < 0) {
+      parent.left = n
+    } else {
+      parent.right = n
+    }
+    fixAfterInsertion(n)
+    //TODO: size += 1
+    return None
+  }
+
+  //////////////// internal implementation
+
+  private def getNode(key: A)(implicit txn: Txn): Node[A,B] = {
+    val k: Ordered[A] = key
+    var p = root
+    while (null != p) {
+      val cmp = k.compare(p.key)
+        if (cmp < 0) {
+          p = p.left
+        } else if (cmp > 0) {
+          p = p.right
+        } else {
+          return p
+        }
+    }
+    return null
+  }
 
   private def successor(x: Node[A,B])(implicit txn: Txn) = {
     if (null == x) {
       null
     } else {
-      var tmp = x.right?;
+      var tmp = x.right
       if (null != tmp) {
         var p = tmp
-        tmp = p.left?;
+        tmp = p.left
         while (null != tmp) {
           p = tmp
-          tmp = p.left?
+          tmp = p.left
         }
         p
       } else {
-        var p = x.parent?;
+        var p = x.parent
         var ch = tmp
-        while (null != p && ch == p.right.?) {
+        while (null != p && ch == p.right) {
           ch = p
-          p = p.parent?;
+          p = p.parent
         }
         p
       }
@@ -48,21 +116,21 @@ class RedBlackTreeMap[A,B] {
     if (null == x) {
       null
     } else {
-      var tmp = x.left?;
+      var tmp = x.left
       if (null != tmp) {
         var p = tmp
-        tmp = p.right?;
+        tmp = p.right
         while (null != tmp) {
           p = tmp
-          tmp = p.right?
+          tmp = p.right
         }
         p
       } else {
-        var p = x.parent?;
+        var p = x.parent
         var ch = tmp
-        while (null != p && ch == p.left.?) {
+        while (null != p && ch == p.left) {
           ch = p
-          p = p.parent?;
+          p = p.parent
         }
         p
       }
@@ -71,80 +139,80 @@ class RedBlackTreeMap[A,B] {
 
 
   private def colorOf(p: Node[A,B])(implicit txn: Txn) = {
-    if (null == p) BLACK else p.color?
+    if (null == p) BLACK else p.color
   }
 
   private def parentOf(p: Node[A,B])(implicit txn: Txn) = {
-    if (null == p) null else p.parent?
+    if (null == p) null else p.parent
   }
 
   private def setColor(p: Node[A,B], c: Boolean)(implicit txn: Txn) {
-    if (null != p) p.color := c
+    if (null != p && p.color != c) p.color = c
   }
 
   private def leftOf(p: Node[A,B])(implicit txn: Txn) = {
-    if (null == p) null else p.left?
+    if (null == p) null else p.left
   }
 
   private def rightOf(p: Node[A,B])(implicit txn: Txn) = {
-    if (null == p) null else p.right?
+    if (null == p) null else p.right
   }
 
   private def rotateLeft(p: Node[A,B])(implicit txn: Txn) {
     if (null != p) {
-      val r = p.right?;
-      val rl = r.left?;
-      p.right := rl
+      val r = p.right
+      val rl = r.left
+      p.right = rl
       if (null != rl) {
-        rl.parent := p
+        rl.parent = p
       }
-      val par = p.parent?;
-      r.parent := par
+      val par = p.parent
+      r.parent = par
       if (null == par) {
-        root := r
-      } else if (par.left.? == p) {
-        par.left := r
+        root = r
+      } else if (par.left == p) {
+        par.left = r
       } else {
-        par.right := r
+        par.right = r
       }
-      r.left := p
-      p.parent := r
+      r.left = p
+      p.parent = r
     }
   }
 
   private def rotateRight(p: Node[A,B])(implicit txn: Txn) {
     if (null != p) {
-      val l = p.left?;
-      val lr = l.right?;
-      p.left := lr
+      val l = p.left
+      val lr = l.right
+      p.left = lr
       if (null != lr) {
-        lr.parent := p
+        lr.parent = p
       }
-      val par = p.parent?;
-      l.parent := par
+      val par = p.parent
+      l.parent = par
       if (null == par) {
-        root := l
-      } else if (par.right.? == p) {
-        par.right := l
+        root = l
+      } else if (par.right == p) {
+        par.right = l
       } else {
-        par.left := l
+        par.left = l
       }
-      l.right := p
-      p.parent := l
+      l.right = p
+      p.parent = l
     }
   }
 
   private def fixAfterInsertion(x0: Node[A,B])(implicit txn: Txn) {
     var x = x0
-    x.color := RED
+    x.color = RED
 
     while (null != x) {
-      val xp = x.parent?;
-      if (null == xp || xp.color.? == RED) {
+      var xp = x.parent
+      if (null == xp || xp.color != RED) {
         // done
         x = null
       } else {
-        val xpp = parentOf(xp)
+        var xpp = parentOf(xp)
         val xppl = leftOf(xpp)
         val xppr = rightOf(xpp)
         if (xp == xppl) {
@@ -155,17 +223,14 @@ class RedBlackTreeMap[A,B] {
             x = xpp
           } else {
             if (x == rightOf(xp)) {
-              rotateLeft(xp)
-              setColor(xpp, BLACK)
-              val xppp = parentOf(xpp)
-              setColor(xppp, RED)
-              rotateRight(xppp)
               x = xp
-            } else {
-              setColor(xp, BLACK)
-              setColor(xpp, RED)
-              rotateRight(xpp)
+              rotateLeft(x)
+              xp = parentOf(x)
+              xpp = parentOf(xp)
             }
+            setColor(xp, BLACK)
+            setColor(xpp, RED)
+            rotateRight(xpp)
           }
         } else {
           if (colorOf(xppl) == RED) {
@@ -175,22 +240,19 @@ class RedBlackTreeMap[A,B] {
             x = xpp
           } else {
             if (x == leftOf(xp)) {
-              rotateRight(xp)
-              setColor(xpp, BLACK)
-              val xppp = parentOf(xpp)
-              setColor(xppp, RED)
-              rotateLeft(xppp)
               x = xp
-            } else {
-              setColor(parentOf(x), BLACK)
-              setColor(parentOf(parentOf(x)), RED)
-              rotateLeft(parentOf(parentOf(x)))
+              rotateRight(x)
+              xp = parentOf(x)
+              xpp = parentOf(xp)
             }
+            setColor(xp, BLACK)
+            setColor(xpp, RED)
+            rotateLeft(xpp)
           }
         }
       }
     }
-    root.?.color := BLACK
+    setColor(root, BLACK)
   }
 
 //  /**
@@ -210,7 +272,7 @@ class RedBlackTreeMap[A,B] {
 //      } // p has 2 children
 //
 //      // Start fixup at replacement node, if it exists.
-//      Node[A,B] replacement = (p.left != null ? p.left : p.right);
+//      Node[A,B] replacement = (p.left != null  p.left : p.right);
 //
 //      if (replacement != null) {
 //          // Link replacement to parent
@@ -311,20 +373,40 @@ class RedBlackTreeMap[A,B] {
 }
 
 
-private class Node[A,B](val key: A, initialValue: B) extends MetaHolder {
+private class Node[A,B](val key: A, initialValue: B, initialParent: Node[A,B]) extends MetaHolder {
   import Node._
 
   @volatile private var _color: Boolean = true
   @volatile private var _value: B = initialValue
-  @volatile private var _parent: Node[A,B] = null
+  @volatile private var _parent: Node[A,B] = initialParent
   @volatile private var _left: Node[A,B] = null
   @volatile private var _right: Node[A,B] = null
 
-  def color = Color(this)
-  def value = Value[B](this)
-  def parent = Parent[A,B](this)
-  def left = Left[A,B](this)
-  def right = Right[A,B](this)
+  def color(implicit txn: Txn): Boolean = colorRef.get
+  def color_=(v: Boolean)(implicit txn: Txn) { colorRef.set(v) }
+  def colorRef = Color(this)
+  
+  def value(implicit txn: Txn): B = valueRef.get
+  def value_=(v: B)(implicit txn: Txn) { valueRef.set(v) }
+  def valueRef = Value[B](this)
+
+  def setValue(v: B)(implicit txn: Txn): Option[B] = {
+    val prev = value
+    value = v
+    Some(prev)
+  }
+
+  def parent(implicit txn: Txn): Node[A,B] = parentRef.get
+  def parent_=(v: Node[A,B])(implicit txn: Txn) { parentRef.set(v) }
+  def parentRef = Parent[A,B](this)
+
+  def left(implicit txn: Txn): Node[A,B] = leftRef.get
+  def left_=(v: Node[A,B])(implicit txn: Txn) { leftRef.set(v) }
+  def leftRef = Left[A,B](this)
+
+  def right(implicit txn: Txn): Node[A,B] = rightRef.get
+  def right_=(v: Node[A,B])(implicit txn: Txn) { rightRef.set(v) }
+  def rightRef = Right[A,B](this)
 }
 
 private object Node {
