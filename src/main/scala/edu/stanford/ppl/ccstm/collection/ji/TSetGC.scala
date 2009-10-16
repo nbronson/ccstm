05eeff1668d03523e344686f27e1207649093529
Nathan Bronson <nbronson@flowertower.(none)>
2009-10-16 00:50:16 -0700

    More work on subtle details.

--- src/main/scala/edu/stanford/ppl/ccstm/collection/ji/TSetGC.scala
+++ src/main/scala/edu/stanford/ppl/ccstm/collection/ji/TSetGC.scala
@@ -1,21 +1,21 @@
 /* CCSTM - (c) 2009 Stanford University - PPL */
 
-// TSet
+// TSetGC
 
 package edu.stanford.ppl.ccstm.collection.ji
 
-import java.lang.ref._
 import java.util.concurrent.ConcurrentHashMap
 import java.util._
+import edu.stanford.ppl.ccstm._
 
 
-object TSet {
+object TSetGC {
   trait Bound[A] extends Set[A] {
-    def unbind: TSet[A]
+    def unbind: TSetGC[A]
     def context: Option[Txn]
   }
 
-  private class TxnBound[A](val unbind: TSet[A], txn: Txn) extends AbstractSet[A] with Bound[A] {
+  private class TxnBound[A](val unbind: TSetGC[A], txn: Txn) extends AbstractSet[A] with Bound[A] {
     def context = Some(txn)
 
     def size: Int = unbind.size(txn)
@@ -25,7 +25,7 @@ object TSet {
 
     def iterator: Iterator[A] = new Iterator[A] {
       private var apparentSize = 0
-      private val iter = unbind._predicates.entrySet.iterator
+      private val iter = unbind._predicates.entrySet().iterator
       private var prev: Option[A] = None
       private var avail: Option[A] = null
 
@@ -45,7 +45,7 @@ object TSet {
           }
         }
         if (apparentSize != size) {
-          txn.forceRollback(Txn.InvalidReadCause(unbind, "TSet.Iterator missed elements"))
+          txn.forceRollback(Txn.InvalidReadCause(unbind, "TSetGC.Iterator missed elements"))
         }
         avail = None
         return
@@ -68,28 +68,31 @@ object TSet {
     }
   }
 
-  private class NonTxnBound[A](val unbind: TSet[A]) extends AbstractSet[A] with Bound[A] {
+  private class NonTxnBound[A](val unbind: TSetGC[A]) extends AbstractSet[A] with Bound[A] {
     def context = None
 
     def size: Int = unbind._size.nonTxn.get
 
     override def contains(key: Any): Boolean = {
-      val ref = unbind._predicates.get(key)
-      null != ref && ref.nonTxn.get == Present
+      containsImpl(unbind._predicates.get(key))
+    }
+
+    private def containsImpl(ref: Ref[State]): Boolean = {
+      null != ref && (ref.nonTxn.get eq Present)
     }
 
     override def add(key: A): Boolean = {
-      // need to atomically update the size and the pred
-      !contains(key) && STM.atomic(t => unbind.add(key)(t))
+      val ref = unbind._predicates.get(key)
+      !containsImpl(ref) && STM.atomic(t => unbind.addImpl(key, ref)(t))
     }
 
     override def remove(key: Any): Boolean = {
-      // need to atomically update the size and the pred
-      contains(key) && STM.atomic(t => unbind.remove(key)(t))
+      val ref = unbind._predicates.get(key)
+      containsImpl(ref) && STM.atomic(t => unbind.removeImpl(key, ref)(t))
     }
 
     def iterator: Iterator[A] = new Iterator[A] {
-      private val iter = unbind._predicates.entrySet.iterator
+      private val iter = unbind._predicates.entrySet().iterator
       private var prev: Option[A] = None
       private var avail: Option[A] = null
 
@@ -126,11 +129,11 @@ object TSet {
 
   object Present extends State
   
-  class Absent(set: TSet[_], key: Any) {
+  class Absent(set: TSetGC[_], key: Any) {
     val observer = new ObservedAbsent(set, key, this)
   }
 
-  case class ObservedAbsent(set: TSet[_], key: Any, absent: Absent) extends CleanableRef[Absent](absent) with State {
+  case class ObservedAbsent(set: TSetGC[_], key: Any, absent: Absent) extends CleanableRef[Absent](absent) with State {
     def cleanup() { set.cleanup(this) }
   }
 
@@ -140,12 +143,16 @@ object TSet {
 }
 
 /** A transactional set that implements a <code>java.util.Set</code>-like
- *  interface.
+ *  interface using Transactional Predication, with garbage collection of empty
+ *  accessed entries performed incrementally by using weak references and a
+ *  shared cleanup thread.  This implementation is serializable, but does not
+ *  provide opacity. Transactional reads may return different values with no
+ *  intervening writes inside a transaction that rolls back. 
  */
-class TSet[A] {
-  import TSet._
+class TSetGC[A] {
+  import TSetGC._
 
-  private val _size = new LazyConflictIntRef(0) // replace with striped version
+  private val _size = new collection.LazyConflictIntRef(0) // replace with striped version
   private val _predicates = new ConcurrentHashMap[Any,Ref[State]]
 
   def size(implicit txn: Txn): Int = _size.get
@@ -154,55 +161,71 @@ class TSet[A] {
   val nonTxn: Bound[A] = new NonTxnBound(this)
 
   def contains(key: Any)(implicit txn: Txn): Boolean = {
+    null != containsImpl(key, _predicates.get(key))
+  }
+
+  private[ji] def createRef(key: Any, initialValue: State)(implicit txn: Txn): Ref[State] = {
+    val fresh = Ref(initialValue)
+    fresh.get
+    fresh
+  }
+
+  /** Returns Ref[Present] or null. */
+  private def containsImpl(key: Any, ref0: Ref[State])(implicit txn: Txn): Ref[State] = {
+    var ref = ref0
     while (true) {
-      val ref = _predicates.get(key)
       if (null == ref) {
         // unobserved-absent, make it observed
         val absent = new Absent(this, key)
-        if (null == _predicates.putIfAbsent(key, Ref(absent.observer))) {
+        if (null == _predicates.putIfAbsent(key, createRef(absent.observer))) {
           txn.addReference(absent)
-          return false
+          return null
         }
       } else {
         ref.get match {
           case Present => {
             // easy
-            return true
+            return ref
           }
           case obs: ObservedAbsent => {
             val absent = obs.get
             if (null != absent) {
               // active observed-absent
               txn.addReference(absent)
-              return false
+              return null
             } else {
               // We must replace the observer, but we can reuse the Ref
-              val fresh = new Absent(this, key)
-              ref.set(fresh.observer)
-              txn.addReference(fresh)
-              return false
+              val absent = new Absent(this, key)
+              ref.set(absent.observer)
+              txn.addReference(absent)
+              return null
             }
           }
           case Invalid => {
             // try to transition directly to unobserved-absent, with a new Ref
             val absent = new Absent(this, key)
-            if (_predicates.replace(key, ref, Ref(absent.observer))) {
+            if (_predicates.replace(key, ref, createRef(absent.observer))) {
               txn.addReference(absent)
-              return false
+              return null
             }
           }
         }
       }
+      ref = _predicates.get(key)
     }
     throw new Error("unreachable")
   }
 
   def add(key: A)(implicit txn: Txn): Boolean = {
+    addImpl(key, _predicates.get(key))
+  }
+
+  private def addImpl(key: A, ref0: Ref[State])(implicit txn: Txn): Boolean = {
+    var ref = ref0
     while (true) {
-      val ref = _predicates.get(key)
       if (null == ref) {
         // unobserved-absent
-        if (null == _predicates.putIfAbsent(key, Ref(Present))) {
+        if (null == _predicates.putIfAbsent(key, createRef(Present))) {
           // success, and not previously present
           _size += 1
           return true
@@ -221,19 +244,28 @@ class TSet[A] {
           }
           case Invalid => {
             // try to transition directly to present, with a new Ref
-            if (_predicates.replace(key, ref, Ref(Present))) {
+            if (_predicates.replace(key, ref, createRef(Present))) {
               _size += 1
               return true
             }
           }
         }
       }
+      ref = _predicates.get(key)
     }
     throw new Error("unreachable")
   }
 
   def remove(key: Any)(implicit txn: Txn): Boolean = {
-    if (contains(key)) {
+    removeImpl(key, _predicates(key))
+  }
+
+  private def removeImpl(key: Any, ref0: Ref[State])(implicit txn: Txn): Boolean = {
+    var ref = containsImpl(key, ref0)
+    if (null != ref) {
+      // TODO: we can't remove immediately
+      // TODO:  option 1: store UnobservedAbsent, defer removal until commit
+      // TODO:  option 2: store ObservedAbsent, let GC remove it later
       _predicates.remove(key)
       _size -= 1
       true
