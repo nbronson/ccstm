b288e6203d72db1519affd01098bb676cae44769
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2009-11-05 13:35:06 -0800

    Switch to Scala 2.7.7 and ScalaTest 1.0.

--- project/build/CCSTMProject.scala
+++ project/build/CCSTMProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class CCSTMProject(info: ProjectInfo) extends DefaultProject(info) {
-  val scalatest = "org.scalatest" % "scalatest" % "1.0-for-scala-2.8.0-SNAPSHOT"
+  val scalatest = "org.scalatest" % "scalatest" % "1.0"
 
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
 }
