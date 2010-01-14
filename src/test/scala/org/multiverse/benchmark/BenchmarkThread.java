33c722d51498dcf5e49800867b78facb400bc1d6
unknown <kuubemasta@.(none)>
2010-01-14 13:58:10 -0800

    added multiverse benchmark

---
+++ src/test/scala/org/multiverse/benchmark/BenchmarkThread.java
@@ -0,0 +1,38 @@
+package org.multiverse.benchmark;
+
+/**
+ * @author Pascal Felber
+ * @since 0.1
+ */
+abstract public class BenchmarkThread implements Runnable {
+
+	volatile private int m_phase;
+	private int m_steps;
+
+	public BenchmarkThread() {
+		m_phase = Benchmark.WARMUP_PHASE;
+		m_steps = 0;
+	}
+
+	public void setPhase(int phase) {
+		m_phase = phase;
+	}
+
+	public int getSteps() {
+		return m_steps;
+	}
+
+	public void run() {
+		while (m_phase == Benchmark.WARMUP_PHASE) {
+			step(Benchmark.WARMUP_PHASE);
+		}
+		while (m_phase == Benchmark.TEST_PHASE) {
+			step(Benchmark.TEST_PHASE);
+			m_steps++;
+		}
+	}
+
+	abstract protected void step(int phase);
+
+	abstract public String getStats();
+}
---
+++ src/test/scala/org/multiverse/benchmark/bank/OverdraftException.java
@@ -0,0 +1,16 @@
+package org.multiverse.benchmark.bank;
+
+/**
+ * @author Pascal Felber
+ * @since 0.1
+ */
+public class OverdraftException extends Exception {
+
+	public OverdraftException() {
+		super();
+	}
+
+	public OverdraftException(String reason) {
+		super(reason);
+	}
+}
