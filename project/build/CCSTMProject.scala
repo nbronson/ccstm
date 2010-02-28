10be541352dcd9bfeec8c1ced87afdbd8b43de69
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2010-02-27 22:33:22 -0800

    SBT build now agrees with IDEA and Maven about directories.

--- project/build/CCSTMProject.scala
+++ project/build/CCSTMProject.scala
@@ -5,4 +5,6 @@ class CCSTMProject(info: ProjectInfo) extends DefaultProject(info) {
   val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-SNAPSHOT"
 
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
+
+  override def outputPath = outputRootPath
 }
