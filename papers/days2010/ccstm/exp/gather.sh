d8dee3e5b684d1e370579e95e13f81f80cdb2e2a
nbronson <nbronson@niagara-3.(none)>
2010-03-13 18:28:54 -0800

    Fix for tl2cm, and the first trickle of results.

--- papers/days2010/ccstm/exp/gather.sh
+++ papers/days2010/ccstm/exp/gather.sh
@@ -7,17 +7,17 @@ if [ ! -x $ECHO ]; then
   AWK=awk
 fi
 
-for m in niagara3; do
+for m in tflop niagara3; do
   $ECHO "$m -------------------"
   for e in cont_low cont_high; do
     for t in threads 1 2 4 8 16 32 64 128 256; do
       $ECHO -n "$t,"
-      for c in ccstm-$e-{d,rfw,t} deuce-$e-{lsa,tl2} multiverse-$e; do
+      for c in ccstm-$e-{d,rfw,t} deuce-$e-{lsacm,tl2cm} multiverse-$e; do
         if [ $t = threads ]; then
           $ECHO -n "$c,"
           continue
         fi
-        egrep '(TOTAL: T|Nb iterations)' pass*/$c-$t.out 2>/dev/null | \
+        egrep '(TOTAL: T|Nb iterations)' $m/pass*/$c-$t.out 2>/dev/null | \
             sed -e 's/^[^=]*= *//' -e 's/,.*//' | \
             $AWK '{x += $1} END {printf("%d,", (NR==0 ? 0 : x/NR))}'
       done
