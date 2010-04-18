fc9016454091e64d0b40fcfcb48e80312752b655
Nathan Grasso Bronson <nbronson@cs.stanford.edu>
2010-04-18 07:44:28 -0700

    Work toward getting cross-compile working under sbt.

--- project/build/CCSTMProject.scala
+++ project/build/CCSTMProject.scala
@@ -3,10 +3,23 @@
 import sbt._
 
 class CCSTMProject(info: ProjectInfo) extends DefaultProject(info) {
-  //val scalatest = "org.scalatest" % "scalatest" % "1.0"
-  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-SNAPSHOT"
-
+  val scalatest = crossScalaVersionString match {
+    case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT"
+    case _ => "org.scalatest" % "scalatest" % ("1.0.1-for-scala-" + crossScalaVersionString + "-SNAPSHOT")
+  }
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
 
-  override def outputPath = outputRootPath
+  // include the license file in the resulting JAR
+  //override def mainResources = super.mainResources + "LICENSE.txt"
+
+  // 2.8.0.RC1 has specialization bugs
+  override def compileOptions =
+    super.compileOptions.toList ++ (if (buildScalaVersion == "2.8.0.RC1") Some(CompileOption("-no-specialization")) else None)
+
+  // this stuff doesn't work right yet
+//  val pplRepo = Resolver.sftp("ppl-sftp-repo", "ppl.stanford.edu", "/var/www/html/ppl/")
+//
+//  override def managedStyle = ManagedStyle.Maven
+//  //val publishTo = "CCSTM Release Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-releases"
+//  val publishTo = "ppl-sftp-repo" at "scp://ppl.stanford.edu/var/www/html/ppl/ccstm/repo-releases"
 }
